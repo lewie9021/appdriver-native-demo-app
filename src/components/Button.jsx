4b04c2a6dcaef118cdc0a0ea721557de1a6db0ee
Lewis Barnes <lb.barnes@live.co.uk>
2019-05-11 18:23:59 +0100

    Refactored Button component.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,20 +2,29 @@ import React from "react";
 import { Text } from "react-native";
 import Pressable from "./Pressable";
 
-const Button = ({text, onPress, ...props}) => {
-  return (
-    <Pressable
-      {...props}
-      style={{
-        padding: 16,
-        backgroundColor: "blue",
-        borderRadius: 2
-      }}
-      onPress={onPress}
-    >
-      <Text style={{fontWeight: "bold", textAlign: "center", color: "white"}}>{text}</Text>
-    </Pressable>
-  )
+const styles = {
+  container: {
+    padding: 16,
+    backgroundColor: "blue",
+    borderRadius: 2
+  },
+  text: {
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white"
+  }
 };
+
+const Button = ({text, onPress, ...props}) => (
+  <Pressable
+    style={styles.container}
+    onPress={onPress}
+    {...props}
+  >
+    <Text style={styles.text}>
+      {text}
+    </Text>
+  </Pressable>
+);
 
 export default Button;
