7de19df1c35532f3b65a09e44fac00f1274d22fe
Lewis Barnes <lb.barnes@live.co.uk>
2020-04-10 15:23:15 +0100

    Removed React Navigation v4.

--- android/app/src/main/java/com/nativedemoapp/MainActivity.java
+++ android/app/src/main/java/com/nativedemoapp/MainActivity.java
@@ -1,9 +1,6 @@
 package com.nativedemoapp;
 
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -16,13 +13,4 @@ public class MainActivity extends ReactActivity {
     return "NativeDemoApp";
   }
 
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-        return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
-  }
 }
